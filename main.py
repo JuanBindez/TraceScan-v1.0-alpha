8d7ff8e606c5c92279c4933704efdaaab785ce2a
JuanBindez <juanbindez780@gmail.com>
2023-04-24 22:25:49 -0300

    update v1.0-dev1 ---> v1.0-dev2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # this is part of the TraceScan project.
 #
-# Release: v1.0-dev1
+# Release: v1.0-dev2
 #
 # Copyright (c) 2023  Juan Bindez  <juanbindez780@gmail.com>
 #
@@ -30,23 +30,62 @@ import whois
 from scapy.all import *
 
 
-def ping():
+
+def traceroute(target):
+    result_text.delete('1.0', END)
+    ttl = 1
+    while True:
+        pkt = IP(dst=target, ttl=ttl) / ICMP()
+        reply = sr1(pkt, verbose=False, timeout=2)
+        if reply is None:
+            result_text.insert(END, f"* * * * \n")
+            break
+        elif reply.type == 11:
+            result_text.insert(END, f"{reply.src} - {round(reply.time*1000, 2)} ms (TTL={reply.ttl})\n")
+        elif reply.type == 0:
+            result_text.insert(END, f"{reply.src} - {round(reply.time*1000, 2)} ms (TTL={reply.ttl})\n")
+            break
+        else:
+            result_text.insert(END, f"{reply.src} - ?? (TTL={reply.ttl})\n")
+            break
+        ttl += 1
+   
+
+def run_traceroute():
+    target = target_entry.get()
+    if target:
+        traceroute(target)
+    else:
+        messagebox.showerror("Error", "Target field is empty")
+
+def ping_google():
     target = "google.com"  # exemplo de alvo, pode ser substituído
-    output = subprocess.run(["ping", "-c", "3", "-n", target], capture_output=True, text=True)
+    output = subprocess.run(["ping", "-c", "23", "-n", target], capture_output=True, text=True)
     result_text.delete('1.0', END)
     result_text.insert(END, f"Ping results for {target}:\n{output.stdout}")
 
 
 window = Tk()
-master = window
-master.title("TraceScan")
-master.geometry("600x700")
+window.title("TraceScan")
+window.geometry("700x650")
 
-ping_button = Button(master, text="Ping", command=ping)
+target_label = Label(window, text="Target:")
+target_label.place(x=20, y=20)
+
+target_entry = Entry(window)
+target_entry.place(x=80, y=20)
+
+run_button = Button(window, text="Run Traceroute", command=run_traceroute)
+run_button.place(x=200, y=20)
+
+result_text = Text(window, height=30)
+result_text.place(x=20, y=100)
+
+ping_button = Button(window, text="Ping Google", command=ping_google)
 ping_button.pack()
 
-result_text = Text(master, height=30)
-result_text.pack()
+result_text = Text(window, height=30)
+result_text.place(x=20, y=100)
 
 
 if __name__ == "__main__":
